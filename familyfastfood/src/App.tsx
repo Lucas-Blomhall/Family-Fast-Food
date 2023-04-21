722ca96a205ac9904db6a164daffc80c10766236
Lucas Blomhäll <lucas.blomhall@gmail.com>
2023-04-21 12:41:35 +0200

    first commit

---
+++ familyfastfood/src/App.tsx
@@ -0,0 +1,13 @@
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import { MsalProvider } from "@azure/msal-react";
+import { useMsal, useIsAuthenticated } from "@azure/msal-react";
+
+import "./App.css";
+
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
